ca19b60f6fc18a6e66ae8069c167708f012984ac
kang-dev95 <harvey.kang.95@proton.me>
2020-05-24 04:52:28 +1000

    Add revision

--- master-install.sh
+++ master-install.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-git clone https://github.com/cezerin/cezerin2
-git clone https://github.com/cezerin/cezerin2-admin
-git clone https://github.com/cezerin/cezerin2-store
+git clone https://github.com/cezerin2/cezerin2
+cd cezerin2
+git checkout fbd272af95d96ae214118c1251267a859bccb7cc
+cd ..
+
+git clone https://github.com/cezerin2/cezerin2-admin
+cd cezerin2-admin
+git checkout 82e112acd4be2cd71fffc8d470db555e9473be77
+cd ..
+
+git clone https://github.com/cezerin2/cezerin2-store
+cd cezerin2-store
+git checkout b05a6697832f9fc50b2d3b8a0aa036138f370ff4
+cd ..
 
 docker-compose run --rm cezerin-api npm install
 
