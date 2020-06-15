153b96b020874aa0751b5235fb91facf86f87412
kang-dev95 <harvey.kang.95@proton.me>
2020-06-15 10:20:15 +1000

    Add caddy

--- master-install.sh
+++ master-install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-git clone https://github.com/cezerin2/cezerin2
+export $(grep -v '^#' .env | xargs)
+
+git clone https://github.com/capfsb/cezerin2
 cd cezerin2
-git checkout 86c08d3b14fd97f8df56443acaaa055bde53fcc5
+git checkout 806142325a8d49fc333d8e9d2fb3a699dbf53a44
 cd ..
 
 git clone https://github.com/cezerin2/cezerin2-admin
@@ -9,9 +11,9 @@ cd cezerin2-admin
 git checkout 82e112acd4be2cd71fffc8d470db555e9473be77
 cd ..
 
-git clone https://github.com/cezerin2/cezerin2-store
+git clone https://github.com/capfsb/cezerin2-store
 cd cezerin2-store
-git checkout b05a6697832f9fc50b2d3b8a0aa036138f370ff4
+git checkout 1fda0948278cd4abf9a66c47c0f412b64e1a2af3
 cd ..
 
 docker-compose run --rm cezerin-api npm install
@@ -21,3 +23,5 @@ docker-compose run --rm cezerin-store npm install --save-dev pm2
 
 docker-compose run --rm cezerin-admin npm install
 docker-compose run --rm cezerin-admin npm install --save-dev pm2
+
+docker-compose up -d
